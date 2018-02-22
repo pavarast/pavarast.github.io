a5cef1a647f6ae32a44a0e0c2c6bd51f8951c784
pavarast <36711308+pavarast@users.noreply.github.com>
2018-02-22 19:30:38 +0000

    Update checkCode.js

--- checkCode.js
+++ checkCode.js
@@ -1,15 +1,11 @@
 function checkCode(theCode) {
     var cCode = document.getElementById('coup');
     var discountCode = "pavara20";
-    var discountCode1 = "pavara30";
  
     if (discountCode == theCode) {
         document.getElementById("disc").value = "20";
         alert("Your order will be " + document.getElementById("disc").value + "% off!");
     }
-    elseif (discountCode1 == theCode) {
-    	document.getElementById("disc").value = "30"
-    	alert ("Your order will be " + document.getElementById("disc").value + "% off!");	
 
     else {
         alert("That is an invalid coupon");
