0e64e80cbe69109c032e19b0bea889c9133c1292
pavarast <36711308+pavarast@users.noreply.github.com>
2018-02-22 19:38:22 +0000

    Update checkCode.js

--- checkCode.js
+++ checkCode.js
@@ -1,9 +1,14 @@
 function checkCode(theCode) {
     var cCode = document.getElementById('coup');
     var discountCode = "pavara20";
+    var discountCode1 = "pavara30";
  
     if (discountCode == theCode) {
         document.getElementById("disc").value = "20";
+        alert("Your order will be " + document.getElementById("disc").value + "% off!");
+    }
+    elseif (discountCode1 == theCode) {
+        document.getElementById("disc").value = "30";
         alert("Your order will be " + document.getElementById("disc").value + "% off!");
     }
 
